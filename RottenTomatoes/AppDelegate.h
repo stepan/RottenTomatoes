157928a722396aff93eca0b1c76fb861b6c0e873
Stepan Grigoryan <stepang@gmail.com>
2014-03-12 23:19:26 -0700

    first commit

---
+++ RottenTomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RottenTomatoes
+//
+//  Created by Stepan Grigoryan on 3/12/14.
+//  Copyright (c) 2014 Stepan Grigoryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
